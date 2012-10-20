d5cccdaf54e7fd2bcfe063eca1b1a925a696700e
Rufus Pollock <rufus.pollock@okfn.org>
2012-10-20 03:58:41 +0100

    [#14,#15,views/load][m]: refactor load page to have UI for loading from GDocs and CSV as well as github (not yet wired up!).

--- src/views/load.js
+++ src/views/load.js
@@ -8,7 +8,9 @@ views.Load = Backbone.View.extend({
   },
 
   _loadDataset: function(e) {
-    var url = $("div.control-group input[name=source]").first().val();
+    e.preventDefault();
+    var $form = $(e.target).closest('form');
+    var url = $form.find("input[name=source]").first().val();
     var project = new models.Project({
       url: url
     });
@@ -17,33 +19,98 @@ views.Load = Backbone.View.extend({
   },
 
   initialize: function(options) {
-    this.el = $(this.el);
+    this.$el = $(this.el);
   },
 
   render: function() {
     var rendered = _.template(this.template, {});
-    $(this.el).html(rendered);
+    this.$el.html(rendered);
     return this;
   },
   
   template: ' \
     <div class="view load"> \
-      <form class="js-load-url form-horizontal"> \
-        <fieldset> \
-          <legend>Load Data</legend> \
-          <div class="control-group"> \
-            <label for="source" class="control-label">CSV file</label> \
-            <div class="controls"> \
-              <input type="text" name="source" class="input span4" placeholder="URL to CSV sheet" value="https://github.com/datasets/transformer-test/blob/master/data/data.csv" /> \
-              <p class="help-block"></p> \
-            </div> \
+      <h2>Load Data</h2> \
+      <div class="tabbable"> \
+        <ul class="nav nav-tabs"> \
+          <li class="active"> \
+            <a href="#gdocs" data-toggle="tab">Google Docs Spreadsheet</a> \
+          </li> \
+          <li> \
+            <a href="#github" data-toggle="tab">GitHub CSV</a> \
+          </li> \
+          <li> \
+            <a href="#csv-disk" data-toggle="tab">Load from CSV on disk</a> \
+          </li> \
+        </ul> \
+        <div class="tab-content"> \
+          <div class="tab-pane active" id="gdocs"> \
+            <form class="form-horizontal"> \
+              <fieldset> \
+                <div class="control-group"> \
+                  <label for="source" class="control-label">URL</label> \
+                  <div class="controls"> \
+                    <input type="text" name="source" class="input span6" placeholder="URL to sheet" /> \
+                    <p class="help-block"> \
+                      To load the spreadsheet you must have "published" it (see: File Menu -> Publish to the Web) \
+                    </p> \
+                  </div> \
+                </div> \
+              </fieldset> \
+              <div class="form-actions"> \
+                <button type="submit" class="btn btn-primary load-dataset">Load</button> \
+              </div> \
+            </form> \
           </div> \
-        </fieldset> \
-        <div class="form-actions"> \
-          <button type="submit" class="btn btn-primary load-dataset">Load</button> \
+          <div class="tab-pane" id="github"> \
+            <form class="form-horizontal"> \
+              <fieldset> \
+                <div class="control-group"> \
+                  <label for="source" class="control-label">GitHub CSV URL</label> \
+                  <div class="controls"> \
+                    <input type="text" name="source" class="input span6" placeholder="URL to CSV on GitHub" value="https://github.com/datasets/transformer-test/blob/master/data/data.csv" /> \
+                  </div> \
+                </div> \
+              </fieldset> \
+              <div class="form-actions"> \
+                <button type="submit" class="btn btn-primary load-dataset">Load</button> \
+              </div> \
+            </form> \
+          </div> \
+          <div class="tab-pane" id="csv-disk"> \
+            <form class="form-horizontal"> \
+              <div class="control-group"> \
+                <label class="control-label">File</label> \
+                <div class="controls"> \
+                  <input type="file" name="source" /> \
+                </div> \
+              </div> \
+              <div class="control-group"> \
+                <label class="control-label">Separator</label> \
+                <div class="controls"> \
+                  <input type="text" name="separator" value="," class="spam1"/> \
+                </div> \
+              </div> \
+              <div class="control-group"> \
+                <label class="control-label">Text delimiter</label> \
+                <div class="controls"> \
+                  <input type="text" name="delimiter" value=\'"\' /> \
+                </div> \
+              </div> \
+              <div class="control-group"> \
+                <label class="control-label">Encoding</label> \
+                <div class="controls"> \
+                  <input type="text" name="encoding" value="UTF-8" /> \
+                </div> \
+              </div> \
+              <div class="form-actions"> \
+                <button type="submit" class="btn btn-primary load-dataset">Load</button> \
+              </div> \
+            </form> \
+          </div> \
         </div> \
-      </form> \
     </div> \
+  </div> \
   '
 });
 
