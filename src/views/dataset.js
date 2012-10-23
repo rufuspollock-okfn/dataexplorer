af500904ffef85673397f8a1c16c6628ef6cdba3
Rufus Pollock <rufus.pollock@okfn.org>
2012-10-23 23:10:21 +0100

    [views/dataset][s]: refactor to fix tabs setup in dataset view / transform view.

--- src/views/dataset.js
+++ src/views/dataset.js
@@ -4,21 +4,6 @@ views.Dataset = Backbone.View.extend({
   id: 'start',
 
   events: {
-    'click .toggle-transform': '_toggleTransform'
-  },
-
-  _toggleTransform: function(e) {
-    var viewName = $(e.target).attr('data-view');
-    this.el.find('.dataset-menu .views a').removeClass('active');
-    this.el.find('.dataset-menu .views a[data-view="' + viewName + '"]').addClass('active');
-    if (viewName == 'grid') {
-  		$('#editor').hide();
-  		$('#grid').show();
-    } else {
-  		$('#grid').hide();
-  		$('#editor').show();
-    }
-  	return false;
   },
 
   initialize: function(options) {
@@ -31,28 +16,28 @@ views.Dataset = Backbone.View.extend({
   },
 
   render: function() {
-    var rendered = _.template(this.template, {
-    	name: this.model.title 
-    });
-    this.el.html(rendered);
+    this.el.html(this.template);
     this.el.find('#grid').empty().append(this.grid.el);
     this.el.find('#editor').empty().append(this.editor.el);
-    this.el.find('.nav-tabs a:last').tab('show');
-    this.el.find('#transformations').show();
     return this;
   },
 
   template: ' \
     <div class="view start"> \
-      <ul id="main-menu" class="nav nav-tabs"> \
-        <li><a class="grid-selector" href="#grid" data-toggle="tab">Grid</a></li> \
-        <li><a href="#transformations" data-toggle="tab">Transformations</a></li> \
-      </ul> \
- \
-      <div class="tab-content"> \
-        <div class="tab-pane" id="grid"></div> \
-        <div class="tab-pane" id="transformations"> \
-          <div id="editor"></div> \
+      <div class="tabbable"> \
+        <ul class="nav nav-tabs"> \
+          <li class="active"> \
+            <a href="#grid" data-toggle="tab" class="grid-selector">Grid</a> \
+            </li> \
+          <li> \
+            <a href="#transformations" data-toggle="tab">Transformations</a> \
+          </li> \
+        </ul> \
+        <div class="tab-content"> \
+          <div class="tab-pane active" id="grid"></div> \
+          <div class="tab-pane" id="transformations"> \
+            <div id="editor"></div> \
+          </div> \
         </div> \
       </div> \
     </div> \
