e0d29678d5ad3bbf3bfe3c6d626a0540709d26aa
Rufus Pollock <rufus.pollock@okfn.org>
2012-11-18 17:08:16 +0000

    [refactor][s]: move 1,2,3,4 overview from login.js view to index.html.

--- src/views/login.js
+++ src/views/login.js
@@ -17,34 +17,6 @@ views.Login = Backbone.View.extend({
   
   template: ' \
     <div class="view login"> \
-      <h1 style="text-align: center; margin: 30px;">How It Works</h1> \
-      <div class="row instructions"> \
-        <div class="span3"> \
-          <div class="well"> \
-            <h3>1. Login</h3> \
-            <p>Login to GitHub so that we can save your work later</p> \
-          </div> \
-        </div> \
-        <div class="span3"> \
-          <div class="well"> \
-            <h3>2. Load Some Data</h3> \
-            <p>Load data from online sources (e.g. the DataHub, GitHub or CSVs online)</p> \
-          </div> \
-        </div> \
-        <div class="span3"> \
-          <div class="well"> \
-            <h3>3. Clean It Up</h2> \
-            <p>Write simple javascript or use the built-in functions to clean up the data.</p> \
-          </div> \
-        </div> \
-        <div class="span3"> \
-          <div class="well"> \
-            <h3>4. Save the Result</h3> \
-            <p>Save the resulting <strong>data and scripts</strong> to GitHub</p> \
-          </div> \
-        </div> \
-      </div> \
-   \
       <div class="splash"> \
         <div class="authorize"> \
           <h1>Get Started</h1> \
