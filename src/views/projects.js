6982ec52cc91844ac6cfe8a13881e14bd035a6df
Rufus Pollock <rufus.pollock@okfn.org>
2012-12-28 20:56:15 +0000

    [view/projects][s]: prettify the projects view.

--- src/views/projects.js
+++ src/views/projects.js
@@ -3,19 +3,40 @@
 views.Projects = Backbone.View.extend({
   template: ' \
     <div class="view projects"> \
-      <h2>Load one of your existing projects or <em><a href="#load">Create a new one &raquo;</a></em></h2> \
+      <div class="page-header"> \
+      <h2>Existing Projects ({{total}})</h2> \
+  </div> \
+      <p> \
+        Not looking for an existing project? <a href="#load">Create a new project by loading some data here &raquo;</a>\
+      </p> \
+      <hr /> \
       {{#projects}} \
       <div class="project summary"> \
-        <h3><a href="#{{id}}" class="js-load-project">{{source.url}}</a></h3> \
-        Last updated: {{last_modified}} \
+        <h3 class="title"><a href="#{{id}}" class="js-load-project">{{showTitle}}</a></h3> \
+        Last modified: {{last_modified_nice}} \
+        <br /> \
+        Data source: {{source.url}} \
       </div> \
       {{/projects}} \
     </div> \
   ',
 
+  initialize: function() {
+    this.collection.bind('add', this.render);
+    this.collection.bind('reset', this.render);
+    this.collection.bind('remove', this.render);
+  },
+
   render: function() {
+    var projects = _.map(this.collection.toJSON(), function(project) {
+      project.last_modified_nice = new Date(project.last_modified).toString();
+      project.showTitle = project.title || project.source.url;
+      console.log(project.showTitle);
+      return project;
+    });
     var tmp = Mustache.render(this.template, {
-      projects: this.collection.toJSON()
+      total: projects.length,
+      projects: projects
     });
     this.$el.html(tmp);
   },
