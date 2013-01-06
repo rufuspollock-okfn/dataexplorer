9cadc640b8e0a7ade446e1cd0758e22215be1d69
Rufus Pollock <rufus.pollock@okfn.org>
2013-01-06 10:05:39 +0000

    [#40,test/model][s]: first passing tests.

--- test/model.test.js
+++ test/model.test.js
@@ -2,8 +2,10 @@
 module("Model");
 
 test('Project: basics', function () {
-  // var project = new models.Project();
+  var project = new DataExplorer.Model.Project({name: 'abc'});
+  equal(project.get('name'), 'abc');
+  equal(project.scripts.length, 1);
+  equal(project.get('manifest_version'), 1);
 });
-
 
 })();
