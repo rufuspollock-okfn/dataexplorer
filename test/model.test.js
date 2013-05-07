63c853b4897b8451a376beb14c36db24c175c9ff
Rufus Pollock <rufus.pollock@okfn.org>
2013-05-07 20:08:48 +0100

    [test/model][s]: get tests running again (@djw not running these i think!).

--- test/model.test.js
+++ test/model.test.js
@@ -66,28 +66,38 @@ test('serializeProject', function () {
   });
   project.loadSourceDataset(function() {});
 
+  // check some basics
+  equal(project.toJSON().readme, readme, 'Check we have not changed the original project');
+  equal(project.toJSON().scripts[0].content, 'xyz', 'Check we have not changed the original project');
+
+
   var dataFile = 'data.csv';
+
   // ==============
   // test serialize
 
   var out = DataExplorer.Model.serializeProject(project);
 
-  equal(project.toJSON().readme, readme, 'Check we have not changed the original project');
-  equal(project.toJSON().scripts[0].content, 'xyz', 'Check we have not changed the original project');
-
   equal(out.description, project.get('name') + ' - ' + readme);
+
+  // check the data packaage
   var dp = JSON.parse(out.files['datapackage.json'].content);
   deepEqual(_.keys(out.files),
-      ['datapackage.json', 'README.md', 'main.js', dataFile]
+      ['datapackage.json', 'README.md', 'main.js']
     );
   ok(!_.hasOwnProperty(dp.scripts[0], 'content'), 'content key should be removed');
+  equal(dp.datasets, undefined, 'on datapackage.json datasets is named resources');
+  equal(dp.files, undefined, 'on datapackage.json datasets is named resources');
+  equal(dp.resources[0].backend, 'csv');
+  equal(dp.resources[0].data, undefined, 'We removed data attribute from the dataset');
+
+  // check the content
   deepEqual(out.files['main.js'].content, 'xyz');
-  deepEqual(out.files['data.csv'].content, csvData);
-  equal(dp.datasets, undefined, 'on datapackage.json datasets is named files');
-  equal(dp.files[0].backend, 'csv');
-  equal(dp.files[0].data, undefined, 'We removed data attribute from the dataset');
+  // no longer including data in basic serialization
+  // deepEqual(out.files['data.csv'].content, csvData);
   equal(out.files['README.md'].content, readme, 'README content correct');
 
+  // make some changes prior to unserializing
   var newScriptContent = 'request("...")';
   var newReadme = 'New readme';
   out.files['main.js'].content = newScriptContent;
@@ -97,7 +107,8 @@ test('serializeProject', function () {
   var newProject = DataExplorer.Model.unserializeProject(out);
   equal(newProject.get('name'), project.get('name'));
   equal(newProject.scripts.at(0).get('content'), newScriptContent);
-  equal(newProject.datasets.at(0).get('data'), csvData);
+  // no longer including data in basic serialization
+  // equal(newProject.datasets.at(0).get('data'), csvData);
   equal(newProject.get('readme'), newReadme, 'readme attribute correct');
 });
 
