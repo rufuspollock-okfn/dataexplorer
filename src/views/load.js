rufuspollock-okfn/dataexplorer

commit 201e3918c7982ac3231133d67bb2e847f87d6773

[#129,load][s]: load of remote CSV operational again via datapipes - fixes #129.

src/views/load.js
@@ -26,6 +26,11 @@ my.Load = Backbone.View.extend({
     } else if (backend === "gdocs") {
       url = recline.Backend.GDocs.getGDocsAPIUrls(url).spreadsheet;
     }
+      // TODO: we could be fancy and first check we can get load from original
+      // URL first (this might matter because of performance and not overloading datapipes)
+      else if (backend === 'csv') {
+      url = 'http://datapipes.okfnlabs.org/csv/raw/?url=' + encodeURIComponent(url);
+    }
 
     $.ajax(url, {
       type: "HEAD",
@@ -57,7 +62,6 @@ my.Load = Backbone.View.extend({
     });
     // try to set name
     if (data.url) {
-
       data.backend = this._guessBackend(data.url);
 
       if (data.backend !== 'gdocs') {
@@ -298,15 +302,30 @@ my.Preview = Backbone.View.extend({
       gdocs: this.model.get("backend") === "gdocs"
     }));
 
-    this.model.fetch().done(function () {
-      var grid = new recline.View.SlickGrid({
-        model: self.model
-      });
+    function fetchIt() {
+      self.model.fetch().done(function () {
+        var grid = new recline.View.SlickGrid({
+          model: self.model
+        });
 
-      grid.render();
-      self.$el.find("#grid").append(grid.el);
-      grid.show();
-    });
+        grid.render();
+        self.$el.find("#grid").append(grid.el);
+        grid.show();
+      });
+    }
+    
+    // HACK: this is kind of hacky
+    // TODO: we should fix up this whole system properly (we probably want to
+    // cache raw data somewhere so as we change config we can reload)
+    if (this.model.get('backend') === 'csv') {
+      var tmpurl = 'http://datapipes.okfnlabs.org/csv/raw/?url=' + encodeURIComponent(this.model.get('url'));
+      $.get(tmpurl, function(data) {
+        self.model.set('data', data);
+        fetchIt();
+      });
+    } else {
+      fetchIt();
+    }
 
     this.$el.find("input[name=title]").val(this.projectName);
     this.$el.find("select[name=delimiter]").val(this.model.get("delimiter"));

commit 2eead6b3b73f68b5aba508b31fffc562386a603a

[load,bugfix][s]: fix for file loading bug introduced in 201e3918c7982ac3231133d67bb2e847f87d6773.

src/views/load.js
@@ -317,7 +317,8 @@ my.Preview = Backbone.View.extend({
     // HACK: this is kind of hacky
     // TODO: we should fix up this whole system properly (we probably want to
     // cache raw data somewhere so as we change config we can reload)
-    if (this.model.get('backend') === 'csv') {
+    if (this.model.get('backend') === 'csv' && this.model.get('url')) {
+      console.log(this.model.get('url'));
       var tmpurl = 'http://datapipes.okfnlabs.org/csv/raw/?url=' + encodeURIComponent(this.model.get('url'));
       $.get(tmpurl, function(data) {
         self.model.set('data', data);