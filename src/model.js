334732e0579d837b688099e9fc12cd26ed5a3fc1
Rufus Pollock <rufus.pollock@okfn.org>
2014-02-21 19:24:44 +0000

    [#154][s]: more work trying to make editable through a grid.
    * edit stuff is there but does not seem to trigger save changes.

--- src/model.js
+++ src/model.js
@@ -24,6 +24,9 @@ my.Project = Backbone.Model.extend({
       state: 'active',
       created: new Date().toISOString(),
       sources: [],
+      profiles: {
+        "dataexplorer": "0.9"
+      },
       scripts: [
         {
           id: 'main.js',
@@ -36,7 +39,10 @@ my.Project = Backbone.Model.extend({
           id: 'grid',
           label: 'Grid',
           // must be in recline.View namespace for the present
-          type: 'SlickGrid'
+          type: 'SlickGrid',
+          state: {
+            gridOptions: gridOptions
+          }
         },
         {
           id: 'graph',
@@ -376,6 +382,8 @@ my.unserializeProject = function(serialized) {
     dp.username = serialized.user.login;
   }
 
+  upgradeDataExplorerProject(dp);
+
   var project = new my.Project(dp);
   project.gist_id = serialized.id;
   project.last_modified = new Date(serialized.updated_at);
@@ -386,6 +394,35 @@ my.unserializeProject = function(serialized) {
 
   return project;
 };
+
+// recline slickgrid grid options for editability
+var gridOptions = {
+  editable: true,
+  enabledAddRow: true,
+  enabledDelRow: true,
+  autoEdit: false,
+  enableCellNavigation: true
+};
+
+// perform upgrades on the structure of the DataExplorer object
+// to support changes in structure over time
+function upgradeDataExplorerProject(project) {
+  // before we had profiles (and hence version that we are implementing)
+  if (!project.profiles) {
+    project.profiles = {
+      dataexplorer: "0.9"
+    }
+  }
+  var version = project.profiles.dataexplorer;
+  // version 0.9 upgrade
+  if (version === "0.9") {
+    _.each(project.views, function(view) {
+      if (view.type === 'SlickGrid') {
+        view.state = _.extend({}, view.state, { gridOptions: gridOptions });
+      }
+    });
+  }
+}
 
 my.ProjectList = Backbone.Collection.extend({
   model: my.Project,
